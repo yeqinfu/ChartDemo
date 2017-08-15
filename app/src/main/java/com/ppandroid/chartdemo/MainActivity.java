c5b35c663f2adf9f105c3226ffeb870469c106a7
yeqinfu <990761790@qq.com>
2017-08-15 11:07:45 +0800

    的

--- app/src/main/java/com/ppandroid/chartdemo/MainActivity.java
+++ app/src/main/java/com/ppandroid/chartdemo/MainActivity.java
@@ -1,12 +1,12 @@
 package com.ppandroid.chartdemo;
 
-import com.ppandroid.app.home.FG_OverView;
-import com.ppandroid.app.home.FG_SecurityCenter;
-import com.ppandroid.chartdemo.base.AC_ContentFG;
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import com.ppandroid.app.home.FG_OverView;
+import com.ppandroid.app.home.FG_SecurityCenter;
+import com.ppandroid.chartdemo.base.AC_ContentFG;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +20,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(AC_ContentFG.createIntent(MainActivity.this, FG_SecurityCenter.class.getName()));
             }
         });
-        findViewById(R.id.button02).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(AC_ContentFG.createIntent(MainActivity.this, FG_OverView.class.getName()));
             }
         });
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(AC_ContentFG.createIntent(MainActivity.this, FG_Wave.class.getName()));
+            }
+        });
+
 
 
     }
